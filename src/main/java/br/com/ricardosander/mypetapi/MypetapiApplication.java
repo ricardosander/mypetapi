f469d45ccc548fc4b1de48b8a4af5575f2b72348
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2018-10-18 23:38:16 -0300

    Initializing project.

---
+++ src/main/java/br/com/ricardosander/mypetapi/MypetapiApplication.java
@@ -0,0 +1,12 @@
+package br.com.ricardosander.mypetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypetapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypetapiApplication.class, args);
+	}
+}
