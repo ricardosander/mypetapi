20d43c82879742e86355ca482d8a5a36447a86ff
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2018-10-20 18:37:39 -0300

    Adding Owner and Pet fields.

--- src/main/java/br/com/ricardosander/mypetapi/entities/Owner.java
+++ src/main/java/br/com/ricardosander/mypetapi/entities/Owner.java
@@ -1,5 +1,6 @@
 package br.com.ricardosander.mypetapi.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +13,42 @@ public class Owner {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
 
+  @Column(name = "nome", length = 150)
+  private String name;
+
+  @Column(name = "nome_secundario", length = 150)
+  private String secundaryName;
+
+  @Column(name = "endereco", length = 200)
+  private String address;
+
+  @Column(name = "bairro", length = 50)
+  private String neighborhood;
+
+  @Column(name = "telefone", length = 11)
+  private String phone;
+
+  @Column(name = "telefone2", length = 11)
+  private String phone2;
+
+  @Column(name = "telefone3", length = 11)
+  private String phone3;
+
+  @Column(name = "telefone4", length = 11)
+  private String phone4;
+
+  @Column(name = "telefone5", length = 11)
+  private String phone5;
+
+  @Column(name = "observacao", columnDefinition = "TEXT")
+  private String comments;
+
+  @Column(name = "saldo_devedor")
+  private Double debt;
+
+  @Column(name = "usuario")
+  private Integer user;
+
   public Owner() {
 
   }
@@ -22,6 +59,102 @@ public class Owner {
 
   public void setId(Integer id) {
     this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getSecundaryName() {
+    return secundaryName;
+  }
+
+  public void setSecundaryName(String secundaryName) {
+    this.secundaryName = secundaryName;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public String getNeighborhood() {
+    return neighborhood;
+  }
+
+  public void setNeighborhood(String neighborhood) {
+    this.neighborhood = neighborhood;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+  public String getPhone2() {
+    return phone2;
+  }
+
+  public void setPhone2(String phone2) {
+    this.phone2 = phone2;
+  }
+
+  public String getPhone3() {
+    return phone3;
+  }
+
+  public void setPhone3(String phone3) {
+    this.phone3 = phone3;
+  }
+
+  public String getPhone4() {
+    return phone4;
+  }
+
+  public void setPhone4(String phone4) {
+    this.phone4 = phone4;
+  }
+
+  public String getPhone5() {
+    return phone5;
+  }
+
+  public void setPhone5(String phone5) {
+    this.phone5 = phone5;
+  }
+
+  public String getComments() {
+    return comments;
+  }
+
+  public void setComments(String comments) {
+    this.comments = comments;
+  }
+
+  public Double getDebt() {
+    return debt;
+  }
+
+  public void setDebt(Double debt) {
+    this.debt = debt;
+  }
+
+  public Integer getUser() {
+    return user;
+  }
+
+  public void setUser(Integer user) {
+    this.user = user;
   }
 
 }
--- src/main/java/br/com/ricardosander/mypetapi/entities/Pet.java
+++ src/main/java/br/com/ricardosander/mypetapi/entities/Pet.java
@@ -1,5 +1,8 @@
 package br.com.ricardosander.mypetapi.entities;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +15,51 @@ public class Pet {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
 
+  @Column(name = "nome", length = 50)
+  private String name;
+
+  @Column(name = "especie", length = 50)
+  private String species;
+
+  @Column(name = "raca", length = 50)
+  private String breed;
+
+  @Column(name = "pelo", length = 50)
+  private String fur;
+
+  @Column(name = "pelagem", length = 50)
+  private String coat;
+
+  @Column(name = "porte", length = 50)
+  private String size;
+
+  @Column(name = "peso", length = 50)
+  private Double weigh;
+
+  @Column(name = "nascimento", length = 50)
+  private Date birth;
+
+  @Column(name = "cadastro", length = 50)
+  private Date register;
+
+  @Column(name = "castrado", length = 50)
+  private boolean castrated;
+
+  @Column(name = "observacoes", columnDefinition = "TEXT")
+  private String comments;
+
+  @Column(name = "sexo", length = 50)
+  private String gender;
+
+  @Column(name = "usuario", length = 50)
+  private Integer user;
+
+  @Column(name = "cliente_pacote", length = 50)
+  private boolean bundle;
+
+  @Column(name = "cliente", length = 50)
+  private Integer owner;
+
   public Pet() {
 
   }
@@ -22,6 +70,126 @@ public class Pet {
 
   public void setId(Integer id) {
     this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getSpecies() {
+    return species;
+  }
+
+  public void setSpecies(String species) {
+    this.species = species;
+  }
+
+  public String getBreed() {
+    return breed;
+  }
+
+  public void setBreed(String breed) {
+    this.breed = breed;
+  }
+
+  public String getFur() {
+    return fur;
+  }
+
+  public void setFur(String fur) {
+    this.fur = fur;
+  }
+
+  public String getCoat() {
+    return coat;
+  }
+
+  public void setCoat(String coat) {
+    this.coat = coat;
+  }
+
+  public String getSize() {
+    return size;
+  }
+
+  public void setSize(String size) {
+    this.size = size;
+  }
+
+  public Double getWeigh() {
+    return weigh;
+  }
+
+  public void setWeigh(Double weigh) {
+    this.weigh = weigh;
+  }
+
+  public Date getBirth() {
+    return birth;
+  }
+
+  public void setBirth(Date birth) {
+    this.birth = birth;
+  }
+
+  public Date getRegister() {
+    return register;
+  }
+
+  public void setRegister(Date register) {
+    this.register = register;
+  }
+
+  public boolean isCastrated() {
+    return castrated;
+  }
+
+  public void setCastrated(boolean castrated) {
+    this.castrated = castrated;
+  }
+
+  public String getComments() {
+    return comments;
+  }
+
+  public void setComments(String comments) {
+    this.comments = comments;
+  }
+
+  public String getGender() {
+    return gender;
+  }
+
+  public void setGender(String gender) {
+    this.gender = gender;
+  }
+
+  public Integer getUser() {
+    return user;
+  }
+
+  public void setUser(Integer user) {
+    this.user = user;
+  }
+
+  public boolean isBundle() {
+    return bundle;
+  }
+
+  public void setBundle(boolean bundle) {
+    this.bundle = bundle;
+  }
+
+  public Integer getOwner() {
+    return owner;
+  }
+
+  public void setOwner(Integer owner) {
+    this.owner = owner;
   }
 
 }
