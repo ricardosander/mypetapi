0c65cb9ea22e586a155cce17491111c24b84984e
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2018-10-25 01:07:41 -0300

    Using optional.

--- src/main/java/br/com/ricardosander/mypetapi/repositories/SessionRepository.java
+++ src/main/java/br/com/ricardosander/mypetapi/repositories/SessionRepository.java
@@ -6,10 +6,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SessionRepository extends JpaRepository<Session, Integer> {
 
   @Query("select s from Session s where s.token =:token")
-  Session findByToken(@Param("token") String token);
+  Optional<Session> findByToken(@Param("token") String token);
 
 }
