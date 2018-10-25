a32021839bef96eb274ce3ef62861c7ba9c6f2c2
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2018-10-25 01:09:39 -0300

    Adding find by user id to repositories.

--- src/main/java/br/com/ricardosander/mypetapi/repositories/OwnerRepository.java
+++ src/main/java/br/com/ricardosander/mypetapi/repositories/OwnerRepository.java
@@ -2,9 +2,16 @@ package br.com.ricardosander.mypetapi.repositories;
 
 import br.com.ricardosander.mypetapi.entities.Owner;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OwnerRepository extends JpaRepository<Owner, Integer> {
+
+  @Query("select o from Owner o where o.user = :userId")
+  List<Owner> findAll(@Param("userId") Integer userId);
 
 }
--- src/main/java/br/com/ricardosander/mypetapi/repositories/PetRepository.java
+++ src/main/java/br/com/ricardosander/mypetapi/repositories/PetRepository.java
@@ -2,9 +2,18 @@ package br.com.ricardosander.mypetapi.repositories;
 
 import br.com.ricardosander.mypetapi.entities.Pet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 import javax.persistence.criteria.CriteriaBuilder;
 
+@Repository
 public interface PetRepository extends JpaRepository<Pet, CriteriaBuilder.In> {
+
+  @Query("select p from Pet p where p.user = :userId")
+  List<Pet> findAll(@Param("userId") Integer userId);
 
 }
